5c4cbbb2774a4b89b39628b1235022201a2e5fb9
thuanhuy2006 <thuanhuyho2006@gmail.com>
2026-05-27 21:39:17 +0700

    Thay đổi API

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -1,5 +1,5 @@
 class AppSettings {
-  static const String otpBackendUrl = "https://script.google.com/macros/s/AKfycbw2JCdsxg88L-QrYcoBXFMD3JRxXApqQb74oDJNMDxsWbh4qLuGiYUuDqYAsi-0gwTYRg/exec";
+  static const String otpBackendUrl = "https://script.google.com/macros/s/AKfycbyZQilpS2ZKsOvHtXwOBFjWnAwgo0ptOXgG00CDRmY-0LSYo5hnZl1wSCBYmda1Cgr1qw/exec";
   static double fontScaleValue = 2.0;
   static double sfxVolumeValue = 40.0;
 
